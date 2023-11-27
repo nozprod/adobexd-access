7e6932051d739d990ac13b4dfc4941aef008dada
Valentin Cazin <valentin.cazin@gmail.com>
2023-11-27 12:49:44 +0100

    Features added
    Feature : resume on exceptions
    Feature : link count and display in console

--- access_script.py
+++ access_script.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException, ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,6 +18,7 @@ def automate(email, username, password, csv_path, group):
     # Load csv
     df = pd.read_csv(csv_path, header=None)
     links = df[0].tolist()
+    total_links = len(links)
 
     # Initialize driver
     driver = webdriver.Chrome()
@@ -26,103 +27,141 @@ def automate(email, username, password, csv_path, group):
     report = []
 
     # Connect to Adobe.com
-    driver.get(links[0])
-    email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'EmailPage-EmailField')))
-    email_field.send_keys(email)
-    email_field.send_keys(Keys.RETURN)
+    max_attempts = 3
+    attempt = 0
+    logged = False
 
-    user_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
-    user_field.send_keys(username)
-
-    password_field = driver.find_element(By.ID, 'password')
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN)
-
-    time.sleep(1)  # Stop for 1 second (needed for login acceptance)
-
-    for link in links[0:]:
-        # Open link
-        driver.get(link)
-
-        status = {"Link": link, "Status": "NOK"}  # Default status is NOK
-        performed = False
-
+    while attempt < max_attempts and not logged:
         try:
-            # Clic on invite button
-            invite_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.inviteButton-I4RMS')))
-            invite_button = driver.find_element(By.CSS_SELECTOR, '.inviteButton-I4RMS')
-            invite_button.click()
+            driver.get(links[0])
+            email_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'EmailPage-EmailField')))
+            email_field.send_keys(email)
+            email_field.send_keys(Keys.RETURN)
 
-            # # Fill in a mail
-            # input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ccx-ss-flex-input-textarea')))
-            # input_field = driver.find_element(By.ID, 'ccx-ss-flex-input-textarea')
-            # input_field.send_keys('valentin.cazin@stellantis.com')
-            # input_field.send_keys(Keys.RETURN)
+            user_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
+            user_field.send_keys(username)
 
-            # Fill in a group 
-            input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ccx-ss-flex-input-textarea')))
-            input_field = driver.find_element(By.ID, 'ccx-ss-flex-input-textarea')
-            input_field.send_keys(group)
-            wait = WebDriverWait(driver, 10)
-            suggestions = wait.until(EC.visibility_of_element_located((By.ID, 'ccx-ss-suggestion-0')))
-            suggestions.click()
+            password_field = driver.find_element(By.ID, 'password')
+            password_field.send_keys(password)
+            password_field.send_keys(Keys.RETURN)
 
-            time.sleep(1)  # Stop for 1 second (needed for invite acceptance, generate error otherwise)
+            time.sleep(1)  # Stop for 1 second (needed for login acceptance)
 
-            # # Select invite mode
-            # mode_dropdown = driver.find_element(By.ID, 'react-spectrum-2203')
-            # mode_dropdown.click()
-            # desired_mode = driver.find_element_by_xpath("//option[text()='Mode_d_invitation']")
-            # desired_mode.click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.inviteButton-I4RMS')))
 
-            # Execute invitation
-            final_invite_button = driver.find_element(By.ID, 'ccx-ss-share-invite-send-btn')
-            final_invite_button.click()
-
-            print(f"Invite sent for {link}")
-            status["Status"] = "OK"
-            performed = True
-
-            # Add status to report
-            report.append(status)
+            logged = True
 
         except TimeoutException:
-            print(f"No invite button found on {link}")
+            print(f"\033[91mLogin failed, trying again\033[0m")
+            attempt += 1
 
-        #Ask access if needed
-        if not performed:
+    if logged:
+        print(f"\033[92mLogin succesfull\033[0m")
+        for index, link in enumerate(links[0:]):
             try:
-                # Try to find the request access button
-                access_button = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, "[data-auto='requestAccessButton']"))
-                )
-                
-                access_button.click()
-                print(f"Access required for {link}")
-                status["Status"] = "NOK - Access required"
+                # Open link
+                driver.get(link)
 
-                # Add status to report
+                status = {"Link": link, "Status": "NOK"}  # Default status is NOK
+                performed = False
+
+                try:
+                    # Clic on invite button
+                    invite_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.inviteButton-I4RMS')))
+                    invite_button = driver.find_element(By.CSS_SELECTOR, '.inviteButton-I4RMS')
+                    invite_button.click()
+
+                    # # Fill in a mail
+                    # input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ccx-ss-flex-input-textarea')))
+                    # input_field = driver.find_element(By.ID, 'ccx-ss-flex-input-textarea')
+                    # input_field.send_keys('valentin.cazin@stellantis.com')
+                    # input_field.send_keys(Keys.RETURN)
+
+                    # Fill in a group 
+                    input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ccx-ss-flex-input-textarea')))
+                    input_field = driver.find_element(By.ID, 'ccx-ss-flex-input-textarea')
+                    input_field.send_keys(group)
+                    wait = WebDriverWait(driver, 10)
+                    suggestions = wait.until(EC.visibility_of_element_located((By.ID, 'ccx-ss-suggestion-0')))
+                    suggestions.click()
+
+                    time.sleep(1)  # Stop for 1 second (needed for invite acceptance, generate error otherwise)
+
+                    # # Select invite mode
+                    # mode_dropdown = driver.find_element(By.ID, 'react-spectrum-2203')
+                    # mode_dropdown.click()
+                    # desired_mode = driver.find_element_by_xpath("//option[text()='Mode_d_invitation']")
+                    # desired_mode.click()
+
+                    # Execute invitation
+                    final_invite_button = driver.find_element(By.ID, 'ccx-ss-share-invite-send-btn')
+                    final_invite_button.click()
+
+                    print(f"\033[92m{index + 1}/{total_links}.\033[0m Invite sent")
+                    status["Status"] = "OK"
+                    performed = True
+
+                    # Add status to report
+                    report.append(status)
+
+                except TimeoutException:
+                    print(f"\033[91m{index + 1}/{total_links}.\033[0m No invite button found")
+
+                #Ask access if needed
+                if not performed:
+                    try:
+                        # Try to find the request access button
+                        access_button = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-auto='requestAccessButton']"))
+                        )
+                        
+                        access_button.click()
+                        print(f"\033[91m{index + 1}/{total_links}.\033[0m Access required")
+                        status["Status"] = "NOK - Access required"
+
+                        # Add status to report
+                        report.append(status)
+
+                    except TimeoutException:
+                        print(f"\033[91m{index + 1}/{total_links}.\033[0m Access already asked")
+                        status["Status"] = "NOK - Access required"
+
+                        # Add status to report
+                        report.append(status)
+                        continue
+
+            except StaleElementReferenceException:
+                print(f"\033[91m{index + 1}/{total_links}.\033[0m StaleElementReference error, opening next link")
+                status["Status"] = "NOK - Error"
                 report.append(status)
+                continue
 
-            except TimeoutException:
-                print(f"Access already asked for {link}")
-                status["Status"] = "NOK - Access required"
+            except ElementClickInterceptedException:
+                print(f"\033[91m{index + 1}/{total_links}.\033[0m ElementClickIntercepted error, opening next link")
+                status["Status"] = "NOK - Error"
+                report.append(status)
+                continue
 
-                # Add status to report
+            except Exception as e:
+                print(f"\033[91m{index + 1}/{total_links}.\033[0m Unexpected error: {e}")
+                status["Status"] = "NOK - Error"
                 report.append(status)
                 continue
 
 
-    # Close browser when done
-    driver.quit()
+        # Close browser when done
+        driver.quit()
 
-    # Generate report
-    date_time_now = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-    report_name = f"{date_time_now}_report.csv"
+        # Generate report
+        date_time_now = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+        report_name = f"{date_time_now}_report.csv"
 
-    df_report = pd.DataFrame(report)
-    df_report.to_csv(report_name, index=False, encoding='utf-8-sig')
-    print("Report generated successfully")
+        df_report = pd.DataFrame(report)
+        df_report.to_csv(report_name, index=False, encoding='utf-8-sig')
+        print("\033[92mReport generated successfully\033[0m")
+
+    else:
+        print(f"\033[91mLogin failed after many attempts\033[0m")
 
 #GUI
 def submit():
